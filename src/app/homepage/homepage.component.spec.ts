b58e8d7d6ffbd53349bb01c917bd068515698dc7
PplAshishPal <124656443+PplAshishPal@users.noreply.github.com>
2023-02-08 21:03:37 +0530

    Added test for homepage (#4)
    * workflow: Added build-test-and-codeql-workflow
    
    * workflow: updated workflow steps for build-and-test
    
    * workflow: updated workflow steps for build-and-test
    
    * style: Fixed formatting in build-and-test file
    
    * workflow: Removed cron from codeql-analysis
    
    * workflow: added npm run test in workflow
    
    * added watch flag to false in ng test
    
    * added browser to chromeHeadless in ng test
    
    * test: Fixed broken test cases for homepage and increased coverage
    
    * chore: Added lint command in package.json
    
    * workflow: Removed Autobuild and updated test command
    
    * chore: Changed fail-fast to true
    
    * chore: Updated lint and test command in package.json

--- src/app/homepage/homepage.component.spec.ts
+++ src/app/homepage/homepage.component.spec.ts
@@ -1,23 +1,42 @@
+import { Router } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { HomepageComponent } from './homepage.component';
+import { FormsModule } from '@angular/forms';
 
 describe('HomepageComponent', () => {
   let component: HomepageComponent;
   let fixture: ComponentFixture<HomepageComponent>;
+  let toastrService: ToastrService;
+  let router = { navigate: jasmine.createSpy('navigate') };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomepageComponent ]
-    })
-    .compileComponents();
+      imports: [ToastrModule.forRoot(), FormsModule],
+      declarations: [HomepageComponent],
+      providers: [ToastrService, { provide: Router, useValue: router }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomepageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    toastrService = TestBed.inject(ToastrService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onSubmit Handler', () => {
+    it('should navigate to instructions if inviteCode present', () => {
+      component.inviteCode = '12345';
+      component.onSubmit();
+      expect(router.navigate).toHaveBeenCalledWith(['/instructions']);
+    });
+    it('should give toast error if inviteCode not present', () => {
+      component.inviteCode = '00aa';
+      component.onSubmit();
+      spyOn(toastrService, 'error').and.callThrough();
+    });
   });
 });
